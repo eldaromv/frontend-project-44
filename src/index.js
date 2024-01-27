bded181cb658ac44a23948a6e57808dbbe68b2fd
Eldar <eldar_osmanov@bk.ru>
2024-01-27 14:52:14 +1000

    comma in gretting

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import readlineSync from 'readline-sync';
 const logicOfGame = (rules, initializeRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
   console.log(rules);
 
   let correctAnswers = 0;
