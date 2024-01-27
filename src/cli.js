4b14549f451951ffbcb63ab6bc60b592f8cc768d
Eldar <eldar_osmanov@bk.ru>
2024-01-27 14:48:39 +1000

    mistakes in brain-games

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export function greetUser() {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 }
